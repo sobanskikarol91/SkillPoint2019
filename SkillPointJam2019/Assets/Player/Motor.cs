620465adec33112b12acaad2eeaac5f9e3747301
Risisttt <sdfdsfsdfs11@gmail.com>
2019-06-14 22:31:23 +0200

    Player atacks and animations of push and slash

---
+++ SkillPointJam2019/Assets/Player/Motor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Moves an object forward
+public class Motor : MonoBehaviour
+{
+
+    public new Rigidbody2D rigidbody;
+    public float movementSpeed;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (rigidbody == null)
+            rigidbody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(rigidbody)
+            rigidbody.AddForce(
+                new Vector2(transform.up.x, transform.up.y)
+                * movementSpeed
+        );
+    }
+
+}
---
+++ SkillPointJam2019/Assets/Player/PlayerAtack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator)), RequireComponent(typeof(InputManager))]
+public class PlayerAtack : MonoBehaviour
+{
+    public string[] skillTags = new string[] { "Slash", "Push" };
+    Animator animator;
+    InputManager input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        input = GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < skillTags.Length; ++i)
+        {
+            
+            animator.SetBool(skillTags[i], input.keyPressed[i]);
+        }
+    }
+}
