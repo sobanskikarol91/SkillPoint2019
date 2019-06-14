6c42856fb92ef561ce8e9bbfc1eeb40a0d74d987
Risisttt <sdfdsfsdfs11@gmail.com>
2019-06-14 21:06:41 +0200

    PlayerMovement

---
+++ SkillPointJam2019/Assets/Player/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] string[] keyInputNames = new string[] { "Fire1", "Fire2" };
+    [SerializeField] string xAxisCode = "Horizontal";
+    [SerializeField] string yAxisCode = "Vertical";
+
+    public float minimalPositionInputStrength = 0.1f;
+    public float minimalDirectionInputStrength = 0.1f;
+    [System.NonSerialized] public Vector2 positionInput;
+    [System.NonSerialized] public bool atMove;
+    [System.NonSerialized] public Vector2 directionInput;
+    [System.NonSerialized] public bool atAim;
+
+    [System.NonSerialized] public bool[] keyPressed = new bool[2];
+
+    private void Update()
+    {
+        Vector2 v = new Vector2(Input.GetAxisRaw(xAxisCode), Input.GetAxisRaw(yAxisCode));
+        atMove = v.sqrMagnitude > minimalPositionInputStrength * minimalPositionInputStrength;
+        if (atMove)
+            positionInput = v.normalized;
+
+        v = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        atAim = v.sqrMagnitude > minimalDirectionInputStrength * minimalDirectionInputStrength;
+        if (atAim)
+            directionInput = v.normalized;
+
+        for (int i = 0; i < keyInputNames.Length; ++i)
+            keyPressed[i] = Input.GetButton(keyInputNames[i]);
+    }
+
+}
---
+++ SkillPointJam2019/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(InputManager))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    [Range(0.0f, 1.0f)] public float rotationSpeed = 1.0f;
+    public bool moveToDirection = true;
+    public bool rotateToDirection = true;
+    Rigidbody2D rb;
+    InputManager input;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<InputManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(moveToDirection && input.atMove)
+            rb.AddForce(input.positionInput * movementSpeed);
+        if(rotateToDirection)
+            rb.rotation = Mathf.LerpAngle(rb.rotation, Vector2.SignedAngle(Vector2.up, input.positionInput), rotationSpeed);
+    }
+}
