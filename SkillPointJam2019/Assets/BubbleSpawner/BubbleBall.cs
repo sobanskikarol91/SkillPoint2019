b900c7d720d2de423b9ed3566afc119321f3686d
adrian_slimak <adek124pl@gmail.com>
2019-06-14 23:52:41 +0200

    Bubbles Spawner

---
+++ SkillPointJam2019/Assets/BubbleSpawner/BubbleBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBall : MonoBehaviour
+{
+    public Vector2 endPosition;
+    public float moveSpeed;
+    public GameObject spawnBubble;
+
+    public void FixedUpdate()
+    {
+        //transform.position = Vector2.Lerp(transform.position, endPosition, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, endPosition, moveSpeed);
+        Debug.Log(Vector2.Distance(transform.position, endPosition));
+        if(Vector2.Distance(transform.position, endPosition) < Mathf.Epsilon)
+        {
+            Instantiate(spawnBubble, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
